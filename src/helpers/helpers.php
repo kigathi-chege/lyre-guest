f828098af0417772c0ac62b403654961b42b7df7
kigathi-chege <chegekigathi@gmail.com>
2025-09-17 22:09:53 +0300

    chore: comment out user event logs

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -3,20 +3,20 @@
 if (!function_exists('after_login')) {
     function after_login($user, $request = null)
     {
-        logger("User logged in", ['user_id' => $user->id]);
+        // logger("User logged in", ['user_id' => $user->id]);
     }
 }
 
 if (!function_exists('after_logout')) {
     function after_logout($user, $request = null)
     {
-        logger("User logged out", ['user_id' => $user->id]);
+        // logger("User logged out", ['user_id' => $user->id]);
     }
 }
 
 if (!function_exists('after_register')) {
     function after_register($user, $request = null)
     {
-        logger("User registered", ['user_id' => $user->id]);
+        // logger("User registered", ['user_id' => $user->id]);
     }
 }
